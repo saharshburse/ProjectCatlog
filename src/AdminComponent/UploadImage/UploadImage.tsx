000cdc0735cd2a6e035416db141db8cce333dd68
Saharsh Burse <saharsh2kjain@gmail.com>
2023-02-23 20:55:55 +0530

    Create type added

--- src/AdminComponent/UploadImage/UploadImage.tsx
+++ src/AdminComponent/UploadImage/UploadImage.tsx
@@ -1,106 +1,122 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './UploadImage.css'
 import { useState } from 'react';
 import { db } from "./../../firebase";
 import { uid } from "uid";
-import { set, ref } from "firebase/database";
-// import {cloudinary} from 'cloudinary'
+import { set, ref, onValue } from "firebase/database";
+
 
 export default function UploadImage() {
 
 
- 
-    const [image,setImage]=useState<File | string>("")
-    const [types,setType]=useState<string[]>([])
-    const [stype,setStype]=useState("")
-    const arr=["Main Door","Mica Door","CNC Groove"];
-    setType(arr);
-    const uuid = uid();
-    // const [uploadres,setUploadres]=useState({})
-    // const [imgUrl,setImgurl]=useState("")
 
- // ===============================Cloudinary=================================== 
-    const dataapi="https://api.cloudinary.com/v1_1/dldfmckou/image/upload";
-    const submitImage= ()=>{
-      const data = new FormData()
-      data.append("file",image)
-      data.append("upload_preset","my_preset")
-      data.append("cloud_name","dldfmckou")
-      data.append("public_id",`${stype}/${uuid}`)
-      // data.append("asset_folder","Main Door")
+  const [image, setImage] = useState<File | string>("")
+  const [types, setType] = useState<string[]>([])
+  const [stype, setStype] = useState("")
 
 
-      fetch(dataapi,{
-        method:"post",
-        body:data
-      })
-      .then((res)=>res.json())
-      .then((data)=>{
+  const uuid = uid();
+
+
+  // ===============================Cloudinary=================================== 
+  const dataapi = "https://api.cloudinary.com/v1_1/dldfmckou/image/upload";
+  const submitImage = () => {
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "my_preset")
+    data.append("cloud_name", "dldfmckou")
+    data.append("public_id", `${stype}/${uuid}`)
+    // data.append("asset_folder","Main Door")
+
+
+    fetch(dataapi, {
+      method: "post",
+      body: data
+    })
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-        // setUploadres(data);
-        // setImgurl(data['secure_url']);
         console.log(data['secure_url']);
-        // console.log(imgUrl);
-
         writeToDatabase(data['secure_url']);
         setImage("");
 
-      }).catch((err)=>{
+      }).catch((err) => {
         console.log(err);
       })
-    }
+  }
 
   // ===============================Cloudinary===================================
- 
+
 
   // ============================FireBase===================================
-    
-  // const handleTodoChange = (e) => {
-  //   setTodo(e.target.value);
-  // };
-    //write
-    const writeToDatabase = (url:string) => {
 
-      
-      set(ref(db, `Image/${uuid}`), {
-        url,
-        uuid,
-        stype
+  //write
+  const writeToDatabase = (url: string) => {
+
+
+    set(ref(db, `Image/${uuid}`), {
+      url,
+      uuid,
+      stype
+    });
+
+
+  };
+
+  //read
+  useEffect(() => {
+
+
+    const dbRef = ref(db, '/Type');
+
+    setType([]);
+    onValue(dbRef, (snapshot) => {
+
+      snapshot.forEach((childSnapshot) => {
+
+        setType((types) => [...types, childSnapshot.val().name])
+        //  console.log(types,'typename',childSnapshot.val().name);
       });
-  
-      // setImgurl("");
-    };
+
+
+    }, {
+      onlyOnce: true
+    });
+
+
+
+  }, []);
 
 
 
 
-  
   // ============================FireBase===================================
 
 
 
   return (
     <div className='uploadMain'>
-        <div className='uploadContainer'>
-          <div className='uploadBox'>
-            <input type="file"  onChange={ (e) =>  setImage(e.target.files![0]) } />
-            <label>Type</label>
-            <select name="" id="" defaultValue="select"  onChange={(e)=>{setStype(e.target.value) }} >
-              <option value="select" disabled>select</option>
+      <div className='uploadContainer'>
+        <div className='uploadBox'>
+          <input type="file" onChange={(e) => setImage(e.target.files![0])} />
+          <label>Type</label>
+          <select name="" id="" defaultValue="select" onChange={(e) => { setStype(e.target.value) }} >
+            <option value="select" disabled>select</option>
             {types.map((type, index) => {
-                                return (
-                                  <option key={index} value={type}>{type}</option>
-                                       )
-                            })}
+              return (
+                <option key={index} value={type}>{type}</option>
+              )
+            })}
 
 
-              
-            </select>
-            <button onClick={submitImage }>Upload</button>
 
-          </div>
-            
+
+          </select>
+          <button onClick={submitImage}>Upload</button>
+
         </div>
+
+      </div>
     </div>
   )
 }
