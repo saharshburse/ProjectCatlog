31a5d195a35c344b10746178a6bb8c2f406dab57
Saharsh Burse <saharsh2kjain@gmail.com>
2023-03-05 14:56:31 +0530

    Respomsive navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,34 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import  menu  from '../../Assets/menu.png'
 import './Navbar.css';
 export default function Navbar() {
+
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
+
     return (
-        <div className='navcontainer'>
-            <div>
-            <Link to="/">
-                <h1>LOGO</h1>
-            </Link>
-            </div>
-           
+        <nav className="nav">
+            <div className='navcontainer'>
+                <div className='logo'>
+                    <Link to="/">
+                        <h1>LOGO</h1>
+                    </Link>
+                </div>
+                <div className="menu-icon" onClick={handleShowNavbar}>
+                    <img src={menu} alt="menu" />
+                </div>
 
-            <div className='navbar'>
-                <Link to="/">
-                    <button>HOME</button>
-                </Link>
-                <Link to="/admin">
-                    <button>ADMIN</button>
-                </Link>
-                <Link to="/login">
-                    <button>LOGIN </button>
-                </Link>
-                <Link to="/CreateAccount">
-                    <button>FORM</button>
-                </Link>
+                <div className={`nav-elements  ${showNavbar && 'active'} navbar`}>
 
-                <Link to="/List">
-                    <button>LIST</button>
-                </Link>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/admin">Admin</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login">Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/list">Catlog</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
     )
 }
